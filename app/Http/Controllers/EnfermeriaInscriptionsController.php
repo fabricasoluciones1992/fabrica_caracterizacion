54df69a02c1d1a04a4678008dfd01e8a85b4a577
judax14 <juandavidsilva18@gmail.com>
2024-04-26 08:50:34 -0600

    changes

--- app/Http/Controllers/EnfermeriaInscriptionsController.php
+++ app/Http/Controllers/EnfermeriaInscriptionsController.php
@@ -57,22 +57,10 @@ class EnfermeriaInscriptionsController extends Controller
             ], 403); 
         }
     }
-    private function calculateIMC($weight, $height)
-    {
-        $imc = $weight / (($height / 100) * ($height / 100));
-        if ($imc < 18.5) {
-            return 'Bajo';
-        } elseif ($imc >= 18.5 && $imc < 25) {
-            return 'Normal';
-        } elseif ($imc >= 25 && $imc < 30) {
-            return 'Sobrepeso';
-        } else {
-            return 'Obesidad';
-        }
-    }
+    
     public function show($id)
     {
-        $enfIn = enfermeria_inscription::find($id);
+        $enfIn = enfermeria_inscription::search($id);
         // $bienestar_news=enfermeria_inscriptionController::Getbienestar_news($id);
 
         if ($enfIn == null) {
